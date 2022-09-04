8f6fba85f1961bef5c86a268e306a2067d0e197a
Mamun Rashid <mamun001@gmail.com>
2022-09-03 22:53:48 -0700

    owasp 9

---
+++ terraform_codes_for_gcp/cloud_armor_policy_actuator_and_owasp_top_9/README.md
@@ -0,0 +1,8 @@
+
+# 
+# This folder has the code that deploys cloud armor security policy that blocks actuators and traffoc that matches OWASP top 9
+#
+
+
+## Tested successfuly
+#
---
+++ terraform_codes_for_gcp/cloud_armor_policy_actuator_and_owasp_top_9/variables.tf
@@ -0,0 +1,76 @@
+
+locals {
+  project = "mrashid-cloud-armor-playground"
+  credentials_file = "/Users/mamunrashid/my.json"
+  region = "us-central1"
+  location = "us-central1-a"
+  zone = "us-central1-f"
+  ip_white_list = ["9.9.9.0/24"]
+}
+
+variable "owasp_rules" {
+  description = "OWASP Rules"
+  default = {
+    rule_lfi = {
+      action = "deny(403)"
+      priority = "1001"
+      expression = "evaluatePreconfiguredExpr('lfi-stable')"
+      description = "OWASP lfi"
+    }
+    rule_methodenforcement = {
+      action = "deny(403)"
+      priority = "1002"
+      expression = "evaluatePreconfiguredExpr('methodenforcement-stable')"
+      description = "OWASP methodenforcement"
+    }
+    rule_protcolattack = {
+      action = "deny(403)"
+      priority = "1003"
+      expression = "evaluatePreconfiguredExpr('protocolattack-stable')"
+      description = "OWASP procolattack"
+    }
+    rule_rce = {
+      action = "deny(403)"
+      priority = "1004"
+      expression = "evaluatePreconfiguredExpr('rce-stable')"
+      description = "OWASP rce"
+    }
+    rule_rfi = {
+      action = "deny(403)"
+      priority = "1005"
+      expression = "evaluatePreconfiguredExpr('rfi-stable')"
+      description = "OWASP rfi"
+    }
+    rule_scannerdetection = {
+      action = "deny(403)"
+      priority = "1006"
+      expression = "evaluatePreconfiguredExpr('scannerdetection-stable')"
+      description = "OWASP scannerdetection"
+    }
+    rule_sessionfixation = {
+      action = "deny(403)"
+      priority = "1007"
+      expression = "evaluatePreconfiguredExpr('sessionfixation-stable')"
+      description = "OWASP sessionfixation"
+    }
+    rule_sqli = {
+      action = "deny(403)"
+      priority = "1008"
+      expression = "evaluatePreconfiguredExpr('sqli-stable')"
+      description = "OWASP sqli"
+    }
+    rule_xss = {
+      action = "deny(403)"
+      priority = "1009"
+      expression = "evaluatePreconfiguredExpr('xss-stable')"
+      description = "OWASP xss"
+    }
+  }
+  type = map(object({
+    action = string
+    priority = string
+    expression = string
+    description = string
+    })
+  )
+}
